a0493b6c5ad67512d65e2cf6050bbc46f772837e
Bhumikagrawal <151652434+Bhumikagrawal@users.noreply.github.com>
2026-01-31 16:37:54 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+
+model = tf.keras.models.load_model("animal_species_model.h5")
+
+class_names = ['cane', 'cavallo', 'elefante', 'farfalla', 'gallina',
+               'gatto', 'mucca', 'pecora', 'ragno', 'scoiattolo']
+
+st.title("🐾 Animal Species Classification")
+st.write("Upload an animal image and the model will predict the species.")
+
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg","jpeg","png"])
+
+if uploaded_file:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+
+    img = image.resize((128, 128))
+    img = np.array(img)/255.0
+    img = np.expand_dims(img, axis=0)
+
+    pred = model.predict(img)
+    cls = class_names[np.argmax(pred)]
+    conf = np.max(pred)*100
+
+    st.success(f"Predicted: {cls}")
+    st.info(f"Confidence: {conf:.2f}%")
